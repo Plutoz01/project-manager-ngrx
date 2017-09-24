77a3bb3af3dbe209b2045a63cd2aa28b092856d1
László Csatlós <plutoz01@gmail.com>
2017-09-24 20:18:03 +0200

    Add some delay for inMemoryCrudRepository to mimic more realistic backend

--- src/app/shared/services/AbstractInMemoryCrudRepository.service.ts
+++ src/app/shared/services/AbstractInMemoryCrudRepository.service.ts
@@ -1,47 +1,44 @@
-import { BehaviorSubject } from 'rxjs/Rx';
 import { Observable } from 'rxjs/Observable';
-
-
-
-
+import { CrudRepository } from '../models/crud-repository.interface';
 
 import { Identifiable } from '../models/identifiable.interface';
-import { CrudRepository } from '../models/crud-repository.interface';
 
 export abstract class AbstractInMemoryCrudRepository<T extends Identifiable<ID>, ID extends string | number>
 	implements CrudRepository<T, ID> {
 
+	returnDelay = 1000;
+
 	protected entitiesById: { [key: string]: T };
 
-	constructor(initialData: T[]) {
+	constructor( initialData: T[] ) {
 		initialData = initialData || [];
-		this.entitiesById = initialData.reduce((acc: Object, current: T) => {
-			acc[current.id as string] = current;
+		this.entitiesById = initialData.reduce( ( acc: Object, current: T ) => {
+			acc[ current.id as string ] = current;
 			return acc;
-		}, {});
+		}, {} );
 	}
 
-	getById(id: ID): Observable<T | null> {
-		const result = this.entitiesById[id as string];
-		return Observable.of(result || null);
+	getById( id: ID ): Observable<T | null> {
+		const result = this.entitiesById[ id as string ];
+		return Observable.of( result || null ).delay( this.returnDelay );
 	}
 
-	exists(id: ID): Observable<boolean> {
-		return this.getById(id).map((entity: T | null) => !!entity);
+	exists( id: ID ): Observable<boolean> {
+		return this.getById( id ).map( ( entity: T | null ) => !!entity );
 	}
 
 	getAll(): Observable<T[]> {
-		return Observable.of(Object.values(this.entitiesById)).delay( 1000 );
+		return Observable.of( Object.values( this.entitiesById ) ).delay( this.returnDelay );
 	}
 
-	create(entity: T): Observable<T> {
-		return Observable.of(entity).map((entityToAdd: T) => {
-			if (!entityToAdd) {
-				throw new Error('Entity have to exists');
+	create( entity: T ): Observable<T> {
+		return Observable.of( entity ).map( ( entityToAdd: T ) => {
+			if ( !entityToAdd ) {
+				throw new Error( 'Entity have to exists' );
 			}
 
-			if (!!this.entitiesById[entityToAdd.id as string]) {
-				throw new Error('entity already exists with id: ' + entityToAdd.id);
+			if ( !!this.entitiesById[ entityToAdd.id as string ] ) {
+				throw new Error( 'entity already exists with id: ' + entityToAdd.id );
 			}
 
 			const newEntity = <T>{
@@ -55,21 +52,21 @@ export abstract class AbstractInMemoryCrudRepository<T extends Identifiable<ID>,
 			};
 
 			return newEntity;
-		});
+		} ).delay( this.returnDelay );
 	}
 
-	update(entity: T): Observable<T> {
-		return Observable.of(entity).map((entityToUpdate: T) => {
-			if (!entityToUpdate) {
-				throw new Error('Entity have to exists');
+	update( entity: T ): Observable<T> {
+		return Observable.of( entity ).map( ( entityToUpdate: T ) => {
+			if ( !entityToUpdate ) {
+				throw new Error( 'Entity have to exists' );
 			}
 
-			if (!entityToUpdate.id) {
-				throw new Error('Entity have to have an existing id');
+			if ( !entityToUpdate.id ) {
+				throw new Error( 'Entity have to have an existing id' );
 			}
 
-			if (!this.entitiesById[entityToUpdate.id as string]) {
-				throw new Error('entity have to exists with id: ' + entityToUpdate.id);
+			if ( !this.entitiesById[ entityToUpdate.id as string ] ) {
+				throw new Error( 'entity have to exists with id: ' + entityToUpdate.id );
 			}
 
 			const clonedEntityToUpdate = <T>{
@@ -82,20 +79,20 @@ export abstract class AbstractInMemoryCrudRepository<T extends Identifiable<ID>,
 			};
 
 			return clonedEntityToUpdate;
-		});
+		} ).delay( this.returnDelay );
 	}
 
-	delete(id: ID): Observable<void> {
-		return Observable.of(undefined)
-			.do(() => {
-				if (!id) {
-					throw new Error('id must not be null');
+	delete( id: ID ): Observable<void> {
+		return Observable.of( undefined )
+			.do( () => {
+				if ( !id ) {
+					throw new Error( 'id must not be null' );
 				}
-				if (!!this.entitiesById[id as string]) {
+				if ( !!this.entitiesById[ id as string ] ) {
 					const { [id as string]: undefined, ...result } = this.entitiesById;
 					this.entitiesById = result;
 				}
-			});
+			} ).delay( this.returnDelay );
 	}
 
 	abstract generateNewId(): ID;
