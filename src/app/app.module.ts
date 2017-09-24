5b41d9472e70edda869f3278d9e9dd00ec6b3397
László Csatlós <plutoz01@gmail.com>
2017-09-24 12:27:54 +0200

    Animate project-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { AppRouting } from './app.routing';
 import { ProjectsModule } from './projects/projects.module';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule( {
 	declarations: [
@@ -15,6 +16,7 @@ import { SharedModule } from './shared/shared.module';
 	imports: [
 		AppRouting,
 		BrowserModule,
+		BrowserAnimationsModule,
 		NgbModule.forRoot(),
 		ProjectsModule,
 		SharedModule
--- src/app/projects/components/project-list/project-list.component.ts
+++ src/app/projects/components/project-list/project-list.component.ts
@@ -1,10 +1,22 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
 import { Project } from '../../models/project.class';
 
 @Component( {
 	selector: 'pm-project-list',
 	templateUrl: './project-list.component.html',
-	styleUrls: [ './project-list.component.scss' ]
+	styleUrls: [ './project-list.component.scss' ],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	animations: [
+		trigger( 'flyIn', [
+			state( 'in', style( { transform: 'translateX(0)' } ) ),
+			transition( 'void => *', [
+				style( { transform: 'translateX(-100%)' } ),
+				animate( 100 )
+			] )
+		] )
+	]
 } )
 export class ProjectListComponent {
 
